0983134830599f6260eb51a99b2a8e6e0156ac5b
Andy Simota <andyiulian.simota@gmail.com>
2024-11-13 15:19:41 +0100

    Add nav and team member template

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 import { teamMembers } from "/db/script.js";
+
+const template = `
+  <div class="">
+    <div class="">
+        <img src="img/male1.png" alt="Marco Bianchi"/>
+    </div>
+    <div class="">
+        <h3>Marco Bianchi</h3>
+        <p>Designer</p>
+        <p>marcobianchi@team.com</p>
+    </div>
+    </div>
+`;
+const prova = document.getElementById("members");
+prova.innerHTML = template;
